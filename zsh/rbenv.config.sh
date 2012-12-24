65a5bca25355e7d0f1da4302fc5f407de8bea1a1
midu <stefano.ballabeni+github@gmail.com>
2012-12-23 20:19:06 -0500

    $HOME instead of absolute path because lol

--- zsh/rbenv.config.sh
+++ zsh/rbenv.config.sh
@@ -1,6 +1,5 @@
 eval "$(rbenv init -)"
-export PATH=~/.rbenv/shims:$PATH
-source ~/.rbenv/completions/rbenv.zsh
+source $HOME/.rbenv/completions/rbenv.zsh
 
 alias 193='rbenv local 1.9.3-p194'
 alias ree='rbenv local ree-1.8.7-2012.02'
