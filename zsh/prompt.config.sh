da26300218ba7b9107670aba4d6ba8cde5cc9ddf
midu <stefano.ballabeni+github@gmail.com>
2016-06-11 12:23:47 -0400

    remove time on the right, add command run time on the left

--- zsh/prompt.config.sh
+++ zsh/prompt.config.sh
@@ -1,1 +1,13 @@
-export RPROMPT='[%*]' #$fg_no_bold[white]%}[%D{%H:%M:%S}]'
+# export RPROMPT='[%*]' #$fg_no_bold[white]%}[%D{%H:%M:%S}]'
+function preexec() {
+  timer=${timer:-$SECONDS}
+}
+
+function precmd() {
+  if [ $timer ]; then
+    timer_show=$(($SECONDS - $timer))
+
+    export PROMPT="$fg_no_bold[yellow]${timer_show}s ${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)"
+    unset timer
+  fi
+}
